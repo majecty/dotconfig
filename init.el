5fb66fa2b27af6f1eb39e95531f86eb0e862b572
Park Juhyung <jh@majecty.com>
2020-03-25 16:59:42 +0900

    init.el파일 백업하기

---
+++ init.el
@@ -0,0 +1,28 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(current-language-environment "UTF-8")
+ '(custom-enabled-themes (quote (adwaita)))
+ '(default-input-method "korean-hangul390")
+ '(package-selected-packages (quote (xclip twittering-mode magit))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#EDEDED" :foreground "#2E3436" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "GOOG" :family "Noto Sans CJK KR")))))
+
+(global-git-commit-mode)
+(xclip-mode 1)
