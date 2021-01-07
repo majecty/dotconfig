8c82fc3d386ee7f11429025f30f000826a3e037b
Park Juhyung <jh@majecty.com>
2021-01-07 18:04:34 +0900

    Use parinfer

--- init.el
+++ init.el
@@ -41,6 +41,26 @@
   :config
   (require 'flycheck-clj-kondo))
 
+(use-package parinfer
+  :ensure t
+  :bind
+  (("C-c ." . parinfer-toggle-mode))
+  :init
+  (progn
+    (setq parinfer-extensions
+          '(defaults       ; should be included.
+             pretty-parens  ; different paren styles for different modes.
+	     ;;            evil           ; If you use Evil.
+	     ;;            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
+	     ;;            paredit        ; Introduce some paredit commands.
+             smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+             smart-yank))   ; Yank behavior depend on mode.
+    (add-hook 'clojure-mode-hook #'parinfer-mode)
+    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
+    (add-hook 'scheme-mode-hook #'parinfer-mode)
+    (add-hook 'lisp-mode-hook #'parinfer-mode)))
+
 (global-git-commit-mode)
 (xclip-mode 1)
 (setq lsp-keymap-prefix "s-l")
