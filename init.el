e1c9b099437fe03a2234a445bf50fb194845d7c0
Park Juhyung <jh@majecty.com>
2021-01-07 18:14:11 +0900

    Use paredit for lisps
    Using paredit we can edit parentheses using simple keys

--- init.el
+++ init.el
@@ -127,3 +127,12 @@
 	    (define-key racket-mode-map (kbd "<f5>") 'racket-run)))
 
 (setq geiser-active-implementations '(chicken))
+(autoload 'enable-paredit-mode "paredit"
+  "Turn on pseudo-structural editing of Lisp code."
+  t)
+(add-hook 'emacs-lisp-mode-hook       'enable-paredit-mode)
+(add-hook 'lisp-mode-hook             'enable-paredit-mode)
+(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'scheme-mode-hook           'enable-paredit-mode)
+(add-hook 'clojure-mode-hook          'enable-paredit-mode)
+
