af7484020c7e6ecd789210b5f22c0072208a1bf5
Park Juhyung <jh@majecty.com>
2021-01-14 20:17:27 +0900

    Use god mode in all window

--- init.el
+++ init.el
@@ -174,15 +174,17 @@
   :config
   (progn
     (god-mode)
-    (global-set-key (kbd "<escape>") #'god-local-mode)
+    (global-set-key (kbd "<escape>") #'god-mode-all)
+    (setq god-exempt-major-modes nil)
+    (setq god-exempt-predicates nil)
+
     (defun my-god-mode-update-cursor ()
       (setq cursor-type (if (or god-local-mode buffer-read-only)
                             'box
 			  'bar)))
-
     (add-hook 'god-mode-enabled-hook #'my-god-mode-update-cursor)
     (add-hook 'god-mode-disabled-hook #'my-god-mode-update-cursor)
-    (define-key god-local-mode-map (kbd "i") #'god-local-mode)
+    (define-key god-local-mode-map (kbd "i") #'god-mode-all)
     (define-key god-local-mode-map (kbd ".") #'repeat)
     (global-set-key (kbd "C-x C-1") #'delete-other-windows)
     (global-set-key (kbd "C-x C-2") #'split-window-below)
