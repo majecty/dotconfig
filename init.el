063da4cacbeda43c7a8c6933fa927a958132c247
Park Juhyung <jh@majecty.com>
2021-01-16 11:21:19 +0900

    Change the shortcut of swipe
    I use isearch to move the cursor. Swiper is an awesome plugin.
    But it does not move the cursor if there are more than an occurence
    in a line. I'll use swipe only when I want to jump a line that is
    not visible in the screen.

--- init.el
+++ init.el
@@ -120,7 +120,7 @@
     (setq enable-recursive-minibuffers t)
     ;; enable this if you want `swiper' to use it
     (setq search-default-mode #'char-fold-to-regexp)
-    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-S-s") 'swiper)
     (global-set-key (kbd "C-c C-r") 'ivy-resume)
     (global-set-key (kbd "<f6>") 'ivy-resume)
     (global-set-key (kbd "M-x") 'counsel-M-x)
