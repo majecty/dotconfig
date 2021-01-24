b3ed7fa1d87e06b2f221c5a8e156edb05dbd6041
Park Juhyung <jh@majecty.com>
2021-01-24 22:15:40 +0900

    Run hydra-move on C-n

--- init.el
+++ init.el
@@ -366,6 +366,22 @@ Frames: _f_rame new  _df_ delete
 (global-set-key (kbd "<f5> w") #'hydra-window/body)
 (global-set-key (kbd "C-x C-o") #'hydra-window/body)
 
+(global-set-key
+ (kbd "C-n")
+ (defhydra hydra-move
+   (:body-pre (next-line))
+   "move"
+   ("n" next-line)
+   ("p" previous-line)
+   ("f" forward-char)
+   ("b" backward-char)
+   ("a" beginning-of-line)
+   ("e" move-end-of-line)
+   ("v" scroll-up-command)
+   ;; Converting M-v to V here by analogy.
+   ("V" scroll-down-command)
+   ("l" recenter-top-bottom)))
+
 (put 'narrow-to-region 'disabled nil)
 
 (provide 'init)
