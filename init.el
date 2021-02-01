e827085c4698ef1058ca079636e4980b3267d39e
Park Juhyung <jh@majecty.com>
2021-02-01 15:02:31 +0900

    Install auto package updator

--- init.el
+++ init.el
@@ -320,6 +320,13 @@
 (use-package avy
   :ensure t)
 
+(use-package auto-package-update
+  :ensure t
+  :config
+  (setq auto-package-update-delete-old-versions t
+        auto-package-update-interval 4)
+  (auto-package-update-maybe))
+
 (use-package undo-tree
   :ensure t
   :config
