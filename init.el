4385a06cb794eedc715b72d3b1f992f41092cd65
Park Juhyung <jh@majecty.com>
2021-02-21 15:42:15 +0900

    Add comments for backup and history configs for Emacs

--- init.el
+++ init.el
@@ -40,15 +40,24 @@
  '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 143 :width normal :foundry "GOOG" :family "Noto Sans Mono CJK KR")))))
 
 ;; related backup
+;; Emacs saves backup files in the same directory as the original file.
+;; The line below changes the backup directory to other directory.
 (setq backup-directory-alist '(("." . "~/.config/emacs/backups")))
+;; The line below makes emacs remove excess backup versions silently
 (setq delete-old-versions -1)
+;; Make Emacs versioning its backup files. I don't know how this affect my workflow.
+;; It seems that this [link](https://www.emacswiki.org/emacs/ForceBackups) is related.
 (setq version-control t)
+;; Backup files in git repository, since we don't commit every changes.
 (setq vc-make-backup-files t)
+;; Move auto saved `#file-name#` files to a directory.
 (setq auto-save-file-name-transforms '((".*" "~/.config/emacs/auto-save-list/" t)))
 
 ;; history
+;; minibuffer history is saved in the file
 (setq savehist-file "~/.config/emacs/savehist")
 (savehist-mode 1)
+;; Do not truncate history
 (setq history-length t)
 (setq history-delete-duplicates t)
 (setq savehist-save-minibuffer-history 1)
