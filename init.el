58f79a2f92215dbfbe06fae467b371134274ba35
Park Juhyung <jh@majecty.com>
2021-02-21 16:28:29 +0900

    Try using hippie-expand
    It will complete file name.

--- init.el
+++ init.el
@@ -468,9 +468,27 @@
   (progn
     (setq default-input-method "korean-hangul3shinp2")))
 
-(use-package recomplete
-  :ensure t
-  :bind ("M-/" . recomplete-dabbrev))
+;; (use-package recomplete
+;;   :ensure t
+;;   :bind ("M-/" . recomplete-dabbrev))
+
+(bind-key "M-/" 'hippie-expand)
+
+(defun sanityinc/dabbrev-friend-buffer (other-buffer)
+  (< (buffer-size other-buffer) (* 1 1024 1024)))
+(setq dabbrev-friend-buffer-function 'sanityinc/dabbrev-friend-buffer)
+(setq hippie-expand-try-functions-list
+      '(yas-hippie-try-expand
+        try-expand-all-abbrevs
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-dabbrev
+        try-expand-dabbrev-from-kill
+        try-expand-dabbrev-all-buffers
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
 
 (add-to-list 'load-path "~/jhconfig/emacs")
 (setq flycheck-emacs-lisp-load-path 'inherit)
