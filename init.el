81a0790673e55bef3851d6f12e65a3471ade91cc
Park Juhyung <jh@majecty.com>
2021-02-21 14:15:42 +0900

    Add history settings
    Copied from https://pages.sachachua.com/.emacs.d/Sacha.html#org74426cc

--- init.el
+++ init.el
@@ -46,6 +46,17 @@
 (setq vc-make-backup-files t)
 (setq auto-save-file-name-transforms '((".*" "~/.config/emacs/auto-save-list/" t)))
 
+;; history
+(setq savehist-file "~/.config/emacs/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
+
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (use-package magit
