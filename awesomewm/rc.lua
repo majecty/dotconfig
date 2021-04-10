ba34f72c4b0a8edd79a295b9521ce0d9c1e96559
Park Juhyung <jh@majecty.com>
2021-04-10 22:06:26 +0900

    Use mod alt j,k to iterate all clients

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -288,20 +288,19 @@ globalkeys = my_table.join(
     awful.key({ modkey, altkey    }, "Right", function () lain.util.tag_view_nonempty(1) end,
               {description = "view  previous nonempty", group = "tag"}),
 
-    -- Default client focus
-    -- todo
-    -- awful.key({ altkey,           }, "j",
-    --     function ()
-    --         awful.client.focus.byidx( 1)
-    --     end,
-    --     {description = "focus next by index", group = "client"}
-    -- ),
-    -- awful.key({ altkey,           }, "k",
-    --     function ()
-    --         awful.client.focus.byidx(-1)
-    --     end,
-    --     {description = "focus previous by index", group = "client"}
-    -- ),
+    awful.key({ modkey, altkey,    }, "j",
+       function ()
+          awful.client.focus.byidx( 1)
+       end,
+       {description = "focus next by index", group = "client"}
+    ),
+    awful.key({ modkey, altkey,    }, "k",
+       function ()
+          awful.client.focus.byidx(-1)
+       end,
+       {description = "focus previous by index", group = "client"}
+    ),
+
 
     -- By direction client focus
     awful.key({ modkey }, "j",
