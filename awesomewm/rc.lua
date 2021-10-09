f2fafe5c98b0e3ea5c26ac138b35c7cc631fc837
Park Juhyung <jh@majecty.com>
2021-10-09 18:14:18 +0900

    🐛 use flameshot for screenshot utility

--- awesomewm/rc.lua
+++ awesomewm/rc.lua
@@ -258,7 +258,7 @@ root.buttons(my_table.join(
 globalkeys = my_table.join(
     -- Take a screenshot
     -- https://github.com/lcpz/dots/blob/master/bin/screenshot
-    awful.key({ modkey }, "p", function() os.execute("screenshot") end,
+    awful.key({ modkey }, "p", function() os.execute("flameshot gui -p /home/juhyung/Pictures/") end,
               {description = "take a screenshot", group = "hotkeys"}),
 
     -- X screen locker
