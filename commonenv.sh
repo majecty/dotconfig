7426f41ad55de8567f05b512654e0de4bfc96d92
ParkJuhyung <majecty@gmail.com>
2025-05-12 10:10:27 +0900

    add yarn bin path

--- commonenv.sh
+++ commonenv.sh
@@ -3,6 +3,7 @@ export PATH=$PATH:~/.cargo/bin/
 export PATH=$PATH:~/bin
 export PATH=$PATH:~/.yarn/bin/
 export PATH=$PATH:$HOME/.config/yarn/global
+export PATH=$PATH:$HOME/.yarn/bin
 export PATH=$PATH:~/bin/dynalist-1.0.5
 export PATH=$PATH:/snap/bin/
 export PATH=$PATH:$HOME/bin/firefox
