2f3db305980a06080e6ab954113ed579e0afd564
juhyung <me@juhyung.dev>
2025-06-27 23:01:48 +0900

    Add which key

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,6 +30,37 @@ else
   vim.keymap.set('n', '<C-k>', '<C-w>k')
   vim.keymap.set('n', '<C-l>', '<C-w>l')
 end
+
+
+if vim.g.neovide then
+  -- 저장
+  vim.keymap.set('n', '<D-s>', ':w<CR>')
+  -- 복사 (비주얼 모드)
+  vim.keymap.set('v', '<D-c>', '"+y')
+  -- 붙여넣기 (노멀/비주얼/커맨드/인서트 모드)
+  vim.keymap.set('n', '<D-v>', '"+p')
+  vim.keymap.set('v', '<D-v>', '"+p')
+  vim.keymap.set('c', '<D-v>', '<C-R>+')
+  vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli')
+
+  vim.opt.expandtab = true      -- Tab을 실제 탭 문자가 아니라 스페이스로 변환
+  vim.opt.tabstop = 2          -- 파일 내 탭 문자의 표시 폭을 2로
+  vim.opt.softtabstop = 2      -- Insert 모드에서 Tab 키를 누를 때 2칸 스페이스로 동작
+  vim.opt.shiftwidth = 2       -- 자동 들여쓰기(`>>`, `<<` 등) 시 2칸 스페이스 사용
+  vim.opt.autoindent = true    -- 자동 들여쓰기
+  vim.opt.smartindent = true   -- 스마트 들여쓰기(선택)
+end
+
+
 require("config.lazy")
 
 vim.api.nvim_echo({{"[nvim/init.lua] loaded", "None"}}, false, {})
+
+if vim.g.vscode then
+else
+  vim.keymap.set('n', '<leader>b', "<cmd>Buffers<cr>", { desc = 'buffers' })
+  vim.keymap.set('n', '<leader>gg', "<cmd>Git<cr>", { desc = 'git' })
+  vim.keymap.set('n', '<leader>,p', function ()
+    vim.cmd('source ' .. vim.fn.expand('~/.config/nvim/lua/user/plugins_notvscode/a.lua'))
+  end, { desc = 'plugins' })
+end
--- nvim/lua/user/plugins_notvscode/a.lua
+++ nvim/lua/user/plugins_notvscode/a.lua
@@ -12,4 +12,23 @@ return {
       'junegunn/fzf.vim',
   },
   { 'tpope/vim-fugitive', },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+    keys = {
+      {
+        "<leader>w",
+        function()
+          local wk = require("which-key")
+          wk.show({ global = false })
+        end,
+        desc = "Buffer Local Keymaps (which-key)",
+      },
+    },
+  }
 }
