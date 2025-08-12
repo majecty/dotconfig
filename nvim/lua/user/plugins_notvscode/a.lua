e8393a4fa4ba8695b47345aed38136c1256c19ea
juhyung <me@juhyung.dev>
2025-08-12 20:36:04 +0900

    Remove gp.vim
    잘 안쓰게 되어서 삭제

--- nvim/lua/user/plugins_notvscode/a.lua
+++ nvim/lua/user/plugins_notvscode/a.lua
@@ -165,47 +165,6 @@ return {
 
   -- lazy.nvim
   {
-    "robitx/gp.nvim",
-    event = "VeryLazy",
-    config = function()
-      local conf = {
-        -- For customization, refer to Install > Configuration in the Documentation/Readme
-        providers = {
-          googleai = {
-            endpoint = "https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}",
-            secret = os.getenv("GP_GOOGLEAI_API_KEY"),
-          },
-        },
-        default_command_agent = "CodeGemini",
-        default_chat_agent = "ChatGemini",
-        agents = {
-          {
-            provider = "googleai",
-            name = "CodeGemini",
-            chat = false,
-            command = true,
-            -- string with model name or table with model name and parameters
-            model = { model = "gemini-2.5-flash", temperature = 0.8, top_p = 1 },
-            system_prompt = require("gp.defaults").code_system_prompt,
-          },
-          {
-            provider = "googleai",
-            name = "ChatGemini",
-            chat = true,
-            command = false,
-            -- string with model name or table with model name and parameters
-            model = { model = "gemini-2.5-flash", temperature = 1.1, top_p = 1 },
-            -- system prompt (use this to specify the persona/role of the AI)
-            system_prompt = require("gp.defaults").chat_system_prompt,
-          },
-        },
-      }
-      require("gp").setup(conf)
-
-      -- Setup shortcuts here (see Usage > Shortcuts in the Documentation/Readme)
-    end,
-  },
-  {
     'stevearc/overseer.nvim',
     event = "VeryLazy",
     opts = {},
