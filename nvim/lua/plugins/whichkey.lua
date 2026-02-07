11517560a85605230e1e78c5dc61a81e27b69d8c
juhyung <jh@juhyung.dev>
2026-02-08 05:29:51 +0900

    update whichkey to use new add() API instead of deprecated register()

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -7,111 +7,115 @@ return {
       local wk = require('which-key')
       wk.setup(opts)
 
-      -- Register keymaps
-      wk.register({
-        ['<leader>'] = {
-          w = {
-            function()
-              wk.show({ global = false })
-            end,
-            'Buffer Local Keymaps',
-          },
-          jh = {
-            name = '+jhui',
-            o = {
-              function()
-                require('jhui.main').open()
-              end,
-              'Open jhui buffer',
-            },
-            r = {
-              function()
-                package.loaded['jhui.main'] = nil
-                require('jhui.main')
-                vim.notify('jhui plugin reloaded', vim.log.levels.INFO)
-              end,
-              'Reload jhui plugin',
-            },
-          },
-          e = {
-            function()
-              require('nvim-tree.api').tree.toggle()
-            end,
-            'Toggle file tree',
-          },
-          s = {
-            name = '+session',
-            s = {
-              function()
-                _G.nvim_session.save()
-              end,
-              'Save session',
-            },
-            l = {
-              function()
-                _G.nvim_session.load_picker()
-              end,
-              'Load session',
-            },
-            r = {
-              function()
-                local cwd = vim.fn.getcwd()
-                _G.nvim_session.save()
-                vim.cmd('sleep 100m')
-                os.execute("cd '" .. cwd .. "' && neovide &")
-                vim.cmd('qa!')
-              end,
-              'Reload with GUI',
-            },
-          },
-          E = {
-            function()
-              vim.cmd('e')
-            end,
-            'Open file',
-          },
-          w = {
-            name = '+window',
-            w = {
-              function()
-                vim.cmd('wincmd =')
-              end,
-              'Equalize splits',
-            },
-          },
-          f = {
-            name = '+file',
-            f = {
-              function()
-                require('fzf-lua').files()
-              end,
-              'Find files',
-            },
-          },
+      -- Add keymaps using the new API
+      wk.add({
+        {
+          '<leader>w',
+          function()
+            wk.show({ global = false })
+          end,
+          desc = 'Buffer Local Keymaps',
         },
-        ['<C-h>'] = {
+        { '<leader>jh', group = '+jhui' },
+        {
+          '<leader>jho',
+          function()
+            require('jhui.main').open()
+          end,
+          desc = 'Open jhui buffer',
+        },
+        {
+          '<leader>jhr',
+          function()
+            package.loaded['jhui.main'] = nil
+            require('jhui.main')
+            vim.notify('jhui plugin reloaded', vim.log.levels.INFO)
+          end,
+          desc = 'Reload jhui plugin',
+        },
+        {
+          '<leader>e',
+          function()
+            require('nvim-tree.api').tree.toggle()
+          end,
+          desc = 'Toggle file tree',
+        },
+        { '<leader>s', group = '+session' },
+        {
+          '<leader>ss',
+          function()
+            _G.nvim_session.save()
+          end,
+          desc = 'Save session',
+        },
+        {
+          '<leader>sl',
+          function()
+            _G.nvim_session.load_picker()
+          end,
+          desc = 'Load session',
+        },
+        {
+          '<leader>sr',
+          function()
+            local cwd = vim.fn.getcwd()
+            _G.nvim_session.save()
+            vim.cmd('sleep 100m')
+            os.execute("cd '" .. cwd .. "' && neovide &")
+            vim.cmd('qa!')
+          end,
+          desc = 'Reload with GUI',
+        },
+        {
+          '<leader>E',
+          function()
+            vim.cmd('e')
+          end,
+          desc = 'Open file',
+        },
+        { '<leader>w', group = '+window' },
+        {
+          '<leader>ww',
+          function()
+            vim.cmd('wincmd =')
+          end,
+          desc = 'Equalize splits',
+        },
+        { '<leader>f', group = '+file' },
+        {
+          '<leader>ff',
+          function()
+            require('fzf-lua').files()
+          end,
+          desc = 'Find files',
+        },
+        {
+          '<C-h>',
           function()
             vim.cmd('wincmd h')
           end,
-          'Move left',
+          desc = 'Move left',
         },
-        ['<C-j>'] = {
+        {
+          '<C-j>',
           function()
             vim.cmd('wincmd j')
           end,
-          'Move down',
+          desc = 'Move down',
         },
-        ['<C-k>'] = {
+        {
+          '<C-k>',
           function()
             vim.cmd('wincmd k')
           end,
-          'Move up',
+          desc = 'Move up',
         },
-        ['<C-l>'] = {
+        {
+          '<C-l>',
           function()
             vim.cmd('wincmd l')
           end,
-          'Move right',
+          desc = 'Move right',
         },
       })
     end,
