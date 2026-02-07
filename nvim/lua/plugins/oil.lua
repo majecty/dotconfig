65082e5971d9e098eef865dea37603ce0f31410e
juhyung <jh@juhyung.dev>
2026-02-07 21:06:30 +0900

    fix: add reliable <leader>- keymap to open oil.nvim
    - Moves keymap to oil.nvim config function for lazy-loaded modular setup

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -2,5 +2,8 @@ return {
   {
     'stevearc/oil.nvim',
     opts = {},
+    config = function()
+      vim.keymap.set('n', '<leader>-', require('oil').open, { desc = 'Oil Directory Browser' })
+    end,
   },
 }
