8281f95771195015af60786a8f07de0ecd51eff1
juhyung <jh@juhyung.dev>
2026-02-07 21:36:55 +0900

    feat(oil.nvim): add keymap for '-' to open Oil

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -9,5 +9,9 @@ return {
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
+  config = function(_, opts)
+    require("oil").setup(opts)
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
   }
 }
