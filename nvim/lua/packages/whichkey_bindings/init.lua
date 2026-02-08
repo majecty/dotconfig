38d34866cd1c9a71b7d524b1f52b5eed73a7d0f6
juhyung <jh@juhyung.dev>
2026-02-08 21:19:48 +0900

    simplify whichkey bindings search with hardcoded list

--- nvim/lua/packages/whichkey_bindings/init.lua
+++ nvim/lua/packages/whichkey_bindings/init.lua
@@ -1,34 +1,31 @@
 local M = {}
 
--- Get all registered whichkey bindings
+-- Simple mapping list to display
 function M.find_bindings()
-  local wk = require('which-key')
+  local bindings = {
+    '<leader>? -> Find whichkey bindings',
+    '- -> Open parent directory',
+    'gp -> Show oil.nvim path',
+    '<leader>ee -> Toggle file tree',
+    '<leader>ss -> Save session',
+    '<leader>sl -> Load session',
+    '<leader>st -> Tmux attach/create session',
+    '<leader>sr -> Reload with GUI',
+    '<leader>ff -> Find files',
+    '<leader>fb -> Find buffers',
+    '<C-h> -> Move left',
+    '<C-j> -> Move down',
+    '<C-k> -> Move up',
+    '<C-l> -> Move right',
+  }
   
-  -- Get the registered mappings
-  local mappings = {}
-  
-  -- Collect all mappings from which-key internal data
-  local spec_index = wk.get_spec()
-  if spec_index then
-    for _, item in ipairs(spec_index) do
-      if item[1] and item[2] then
-        local key = item[1]
-        local desc = item[3] and item[3].desc or 'No description'
-        table.insert(mappings, key .. ' -> ' .. desc)
-      end
-    end
-  end
-  
-  if #mappings == 0 then
+  if #bindings == 0 then
     vim.notify('No whichkey bindings found', vim.log.levels.WARN)
     return
   end
   
-  -- Sort mappings
-  table.sort(mappings)
-  
   -- Show with fzf
-  require('fzf-lua').fzf_exec(mappings, {
+  require('fzf-lua').fzf_exec(bindings, {
     prompt = 'Which-key Bindings> ',
     actions = {
       default = function(selected)
