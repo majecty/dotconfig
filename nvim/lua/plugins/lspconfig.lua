12f402c27ea43d9cba66eb3b8966cca1771f3219
juhyung <jh@juhyung.dev>
2026-02-08 21:14:33 +0900

    use vim.lsp.config instead of deprecated lspconfig framework

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -2,15 +2,17 @@ return {
   {
     'neovim/nvim-lspconfig',
     config = function()
-      local lspconfig = require('lspconfig')
-      
       -- Enable LSP info logging
       vim.lsp.set_log_level('info')
       
-      -- Configure TypeScript Language Server (ts_ls)
-      lspconfig.ts_ls.setup({
+      -- Configure TypeScript Language Server using vim.lsp.config
+      vim.lsp.config('ts_ls', {
         cmd = { '/home/juhyung/.local/share/fnm/node-versions/v24.11.1/installation/bin/typescript-language-server', '--stdio' },
+        filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
       })
+      
+      -- Enable typescript language server for appropriate file types
+      vim.lsp.enable('ts_ls')
     end,
   },
 }
