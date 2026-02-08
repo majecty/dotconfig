53651fb4210c70282a5b3da5efd56d7db42cd95a
juhyung <jh@juhyung.dev>
2026-02-08 18:15:24 +0900

    remove tabby plugin, keep tab navigation keymaps

--- nvim/lua/plugins/tabs.lua
+++ nvim/lua/plugins/tabs.lua
@@ -1,40 +1,10 @@
 return {
+  -- H/L keymaps for tab navigation
   {
-    "nvim-tree/nvim-web-devicons",
-    lazy = true,
-  },
-  {
-    "nanozuki/tabby.nvim",
-    event = "VimEnter",
-    config = function()
-      require("tabby.tabline").set(function(line)
-        return {
-          hl = "TabLine",
-          line.wins_in_tab(line.cur_tab),
-          line.sep(" ", "TabLine", "TabLine"),
-          hl = "TabLineFill",
-          line.tabs(),
-          hl = "TabLine",
-          line.sep(" ", "TabLine", "TabLine"),
-          {
-            hl = "TabLine",
-            " ",
-            on_click = function()
-              vim.cmd("tabnew")
-            end,
-          },
-        }
-      end, {
-        buf_name = {
-          mode = "unique",
-        },
-        current_tab_sign = "",
-        inactive_tab_sign = "",
-      })
-
-      -- H/L keymaps for tab navigation
-      vim.keymap.set("n", "H", "<cmd>tabprevious<cr>", { noremap = true, silent = true })
-      vim.keymap.set("n", "L", "<cmd>tabnext<cr>", { noremap = true, silent = true })
-    end,
+    "LazyVim/LazyVim",
+    keys = {
+      { "H", "<cmd>tabprevious<cr>", desc = "Previous tab" },
+      { "L", "<cmd>tabnext<cr>", desc = "Next tab" },
+    },
   },
 }
