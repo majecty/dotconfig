ce8849660026e1a2cf85fddac437284c85775f79
juhyung <jh@juhyung.dev>
2026-02-08 22:16:09 +0900

    fix copilot C-k keymap with expr and replace_keycodes

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -8,7 +8,8 @@ return {
         replace_keycodes = false,
       })
       vim.keymap.set('i', '<C-K>', 'copilot#Next()', {
-        silent = true,
+        expr = true,
+        replace_keycodes = false,
       })
       vim.g.copilot_no_tab_map = true
     end,
