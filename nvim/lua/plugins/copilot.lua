aaa06b5c7a46bc1228ec85d988e5446598a94c52
juhyung <jh@juhyung.dev>
2026-02-17 23:18:41 +0900

    refactor: change copilot plugin laziness to VeryLazy
    - Change from event = 'InsertEnter' to lazy = 'VeryLazy'
    - Plugin loads during idle time instead of on insert mode entry
    - Improves startup performance by deferring plugin loading

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,7 +1,7 @@
 return {
   {
     'github/copilot.vim',
-    event = 'InsertEnter',
+    lazy = 'VeryLazy',
     config = function()
       vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
         expr = true,
