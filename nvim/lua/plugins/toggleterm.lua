cf662654ee577ff65ec36f501d521004831ece47
juhyung <jh@juhyung.dev>
2026-02-18 10:01:52 +0900

    auto-increment terminal id on C-~ keypress

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -15,18 +15,22 @@ return {
 
       -- Create new terminal with auto-incrementing id using C-~
       local toggleterm = require('toggleterm')
+      local terms = require('toggleterm.terminal')
 
-      vim.keymap.set('n', '<C-~>', function()
-        toggleterm.toggle()
-      end, { noremap = true, silent = true, desc = 'Add new terminal' })
+      local function add_new_terminal()
+        local all_terms = terms.get_all(true)
+        local next_id = 1
+        for _, term in pairs(all_terms) do
+          if term.id >= next_id then
+            next_id = term.id + 1
+          end
+        end
+        toggleterm.toggle(next_id)
+      end
 
-      vim.keymap.set('i', '<C-~>', function()
-        toggleterm.toggle()
-      end, { noremap = true, silent = true, desc = 'Add new terminal' })
-
-      vim.keymap.set('t', '<C-~>', function()
-        toggleterm.toggle()
-      end, { noremap = true, silent = true, desc = 'Add new terminal' })
+      vim.keymap.set('n', '<C-~>', add_new_terminal, { noremap = true, silent = true, desc = 'Add new terminal' })
+      vim.keymap.set('i', '<C-~>', add_new_terminal, { noremap = true, silent = true, desc = 'Add new terminal' })
+      vim.keymap.set('t', '<C-~>', add_new_terminal, { noremap = true, silent = true, desc = 'Add new terminal' })
     end,
   },
 }
