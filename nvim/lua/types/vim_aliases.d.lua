dbad46f47a8840137543afc7ecdb50720ca904ee
juhyung <jh@juhyung.dev>
2026-02-18 09:45:20 +0900

    add common vim type aliases

---
+++ nvim/lua/types/vim_aliases.d.lua
@@ -0,0 +1,9 @@
+--- Common type aliases for Neovim
+
+---@alias BufferHandle integer
+---@alias WindowHandle integer
+---@alias TabHandle integer
+---@alias ExtmarkId integer
+---@alias NamespaceId integer
+
+return {}
