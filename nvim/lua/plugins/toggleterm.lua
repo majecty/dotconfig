dbac7f45b559f9d6da90532fd63783fb33a7204a
juhyung <jh@juhyung.dev>
2026-02-18 09:53:47 +0900

    enable terminal mappings in toggleterm for C-` key in all modes

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -6,6 +6,8 @@ return {
     config = function()
       require('toggleterm').setup({
         size = 20,
+        insert_mappings = true,
+        terminal_mappings = true,
         open_mapping = [[<C-`>]],
         direction = 'horizontal',
         shade_terminals = false,
