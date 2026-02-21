a00fe245a1cc55671a3ad4a8588f8f74f65f0632
juhyung <jh@juhyung.dev>
2026-02-21 19:59:24 +0900

    feat: add register preview window in fzf

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -6,7 +6,97 @@ return {
     config = function()
       local fzf = require('fzf-lua')
 
-      -- Set fzf-lua as default UI for vim.ui.select
+      ---@class FzfRegisterPicker
+      local M = {}
+
+      M.win = nil
+      M.buf = nil
+
+      M.show = function()
+        if M.win and vim.api.nvim_win_is_valid(M.win) then
+          return
+        end
+
+        local lines = { 'Registers:', '' }
+        local regs = {
+          '0',
+          '1',
+          '2',
+          '3',
+          '4',
+          '5',
+          '6',
+          '7',
+          '8',
+          '9',
+          'a',
+          'b',
+          'c',
+          'd',
+          'e',
+          'f',
+          'g',
+          'h',
+          'i',
+          'j',
+          'k',
+          'l',
+          'm',
+          'n',
+          'o',
+          'p',
+          'q',
+          'r',
+          's',
+          't',
+          'u',
+          'v',
+          'w',
+          'x',
+          'y',
+          'z',
+          '"',
+          '+',
+          '*',
+        }
+
+        for _, reg in ipairs(regs) do
+          local content = vim.fn.getreg(reg)
+          if content and content ~= '' then
+            content = content:gsub('\n', '\\n'):sub(1, 40)
+            table.insert(lines, string.format('  "%s: %s', reg, content))
+          end
+        end
+
+        M.buf = vim.api.nvim_create_buf(false, true)
+        vim.api.nvim_buf_set_lines(M.buf, 0, -1, false, lines)
+        vim.api.nvim_set_option_value('bufhidden', 'wipe', { buf = M.buf })
+
+        local width = 50
+        local height = math.min(#lines, 15)
+        local col = vim.o.columns - width - 2
+        local row = vim.o.lines - height - 4
+
+        M.win = vim.api.nvim_open_win(M.buf, false, {
+          relative = 'editor',
+          row = row,
+          col = col,
+          width = width,
+          height = height,
+          style = 'minimal',
+          border = 'rounded',
+          title = ' Registers ',
+          title_pos = 'center',
+        })
+      end
+
+      M.hide = function()
+        if M.win and vim.api.nvim_win_is_valid(M.win) then
+          vim.api.nvim_win_close(M.win, true)
+          M.win = nil
+        end
+      end
+
       fzf.setup({
         ui_select = {
           enabled = true,
@@ -21,7 +111,10 @@ return {
         winopts = {
           on_create = function()
             vim.keymap.set('t', '<C-r>', function()
-              return '<C-\\><C-N>"' .. vim.fn.nr2char(vim.fn.getchar()) .. 'pi'
+              M.show()
+              local char = vim.fn.nr2char(vim.fn.getchar())
+              M.hide()
+              return '<C-\\><C-N>"' .. char .. 'pi'
             end, { expr = true, buffer = true })
           end,
         },
