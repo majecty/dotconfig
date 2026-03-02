45dca120d81bdf28e8bb6d53746449ff40aeb796
juhyung <jh@juhyung.dev>
2026-03-02 14:35:06 +0900

    normal 모드에서 Alt-Esc 입력 시 터미널에 ESC 두 번 자동 전송 기능 추가

--- nvim/lua/packages/terminal/init.lua
+++ nvim/lua/packages/terminal/init.lua
@@ -5,6 +5,15 @@ function M.setup()
   -- Terminal mode keybindings
   vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { noremap = true })
   vim.keymap.set('t', '<S-Esc>', '<Esc>', { noremap = true })
+
+  -- Normal mode Alt-Esc: switch to last terminal and send Esc
+  vim.keymap.set('n', '<A-Esc>', function()
+    vim.cmd('startinsert')
+    local chan_id = vim.b.terminal_job_id
+    if chan_id then
+      vim.api.nvim_chan_send(chan_id, '\27\27') -- send ESC twice
+    end
+  end, { noremap = true, silent = true })
 end
 
 return M
