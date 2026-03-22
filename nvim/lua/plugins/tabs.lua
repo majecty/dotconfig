4109a7a140c11fc91c964572ebd0226c64335963
juhyung <jh@juhyung.dev>
2026-03-22 20:10:53 +0900

    feat(nvim): prevent creating new tabs on empty buffers

--- nvim/lua/plugins/tabs.lua
+++ nvim/lua/plugins/tabs.lua
@@ -1,10 +1,33 @@
+local function is_empty_buffer()
+  return vim.fn.bufname("%") == "" and vim.bo.modified == false
+end
+
+local function tab_prev_or_new()
+  local current = vim.fn.tabpagenr()
+  if current > 1 then
+    vim.cmd("tabprevious")
+  elseif not is_empty_buffer() then
+    vim.cmd("tabnew | setlocal buftype=nofile bufhidden=hide noswapfile")
+  end
+end
+
+local function tab_next_or_new()
+  local current = vim.fn.tabpagenr()
+  local total = vim.fn.tabpagenr("$")
+  if current < total then
+    vim.cmd("tabnext")
+  elseif not is_empty_buffer() then
+    vim.cmd("tabnew | setlocal buftype=nofile bufhidden=hide noswapfile")
+  end
+end
+
 return {
   -- H/L keymaps for tab navigation
   {
     "LazyVim/LazyVim",
     keys = {
-      { "H", "<cmd>tabprevious<cr>", desc = "Previous tab" },
-      { "L", "<cmd>tabnext<cr>", desc = "Next tab" },
+      { "H", tab_prev_or_new, desc = "Previous tab (or new if none)" },
+      { "L", tab_next_or_new, desc = "Next tab (or new if none)" },
     },
   },
 }
